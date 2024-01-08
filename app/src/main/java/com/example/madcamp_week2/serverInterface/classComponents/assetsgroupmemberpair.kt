ea9fc61a7a9dd28d7186893df6db11c80093dff8
sgn08050 <sgn08050@kaist.ac.kr>
2024-01-08 21:10:20 +0900

    feat: post assetsgroupmemberpair

---
+++ app/src/main/java/com/example/madcamp_week2/serverInterface/classComponents/assetsgroupmemberpair.kt
@@ -0,0 +1,6 @@
+package com.example.madcamp_week2.serverInterface.classComponents
+
+data class assetsgroupmemberpair (
+    val assetsgroup_id: String,
+    val member: String
+)
---
+++ app/src/main/java/com/example/madcamp_week2/serverInterface/components/POST/assetsgroupmemberpairPost.kt
@@ -0,0 +1,44 @@
+package com.example.madcamp_week2.serverInterface.components.POST
+
+
+import android.util.Log
+import androidx.navigation.NavController
+import com.example.madcamp_week2.ViewModel.memberViewModel
+import com.example.madcamp_week2.serverInterface.ResponseDC
+import com.example.madcamp_week2.serverInterface.classComponents.assetsgroupmemberpair
+import com.example.madcamp_week2.serverInterface.serverAPIInterface
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+fun assetsgroupmemberpairPost(navController: NavController, memberViewModel: memberViewModel, member: String){
+
+    val url = "http://172.10.8.235"
+
+    val retrofit = Retrofit
+        .Builder()
+        .baseUrl(url)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    var server = retrofit.create(serverAPIInterface::class.java)
+
+    val group_id = memberViewModel.group_id.value
+    Log.d("Check098", "Comes here")
+    group_id?.let {
+        Log.d("Check098", "Not null group id")
+        server.postAssetsGroupMemberPair(assetsgroupmemberpair(it, member)).enqueue(object : Callback<ResponseDC> {
+            override fun onResponse(call: Call<ResponseDC>, response: Response<ResponseDC>) {
+                if (response.isSuccessful) {
+                } else {
+                }
+            }
+            override fun onFailure(call: Call<ResponseDC>, t: Throwable) {
+            }
+        })
+    }
+    Log.d("Check098", "Pass")
+}
